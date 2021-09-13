67f620689b92108e3a653c80f55fd57955cb451c
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-13 12:05:13 -0400

    Final Commit

--- src/Password_Validation.java
+++ src/Password_Validation.java
@@ -3,8 +3,25 @@
  *  Copyright 2021 Ronald Lemus
  */
 
+import java.util.Scanner;
+
 public class Password_Validation {
     public static void main(String [] arg){
-        
+        System.out.print("What is the username? ");
+        Scanner userInp = new Scanner(System.in);
+        String user = userInp.nextLine();
+
+        System.out.print("What is the password? ");
+        Scanner passInp = new Scanner(System.in);
+        String password = passInp.nextLine();
+
+        String realPass = "Password123";
+
+        if(password.equals(realPass)){
+            System.out.printf("Welcome %s!", user);
+        }
+        else{
+            System.out.print("I don't know you.");
+        }
     }
 }
